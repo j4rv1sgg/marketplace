4b8894bbf06ee24f68d4eab53e5bf81b7fd32a7e
j4rv1sgg <jrvs.inc@gmail.com>
2023-07-11 14:16:36 +0200

    added profile section

--- src/components/screens/car-edit/CarEdit.jsx
+++ src/components/screens/car-edit/CarEdit.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Navigation from "../home/nav/Navigation.jsx";
 import AddCar from "../home/add-car/AddCar.jsx";
 import {useParams} from "react-router-dom";
 import {getListItem} from "../../services/firebaseService.js";
 
 const CarEdit = () => {
+    const [car, setCar] = useState(null);
     const { id } = useParams()
-    let result
-    const getCar = async () => {
-        result = await getListItem('cars', id)
-        return result
-    }
 
-    console.log(result)
+    useEffect(() => {
+        const getCar = async () => {
+            const result = await getListItem('cars', id);
+            setCar(result);
+        };
+
+        getCar();
+    }, [id]);
+
+
 
     return (
         <div>
-            <AddCar car={getCar()}/>
+            {car && <AddCar car={car}/>}
         </div>
     );
 };
